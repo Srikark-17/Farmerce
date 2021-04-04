8ea97c1bd968286d7233b00ec9b39b3500360a49
Sidharth Anand <44172551+SidharthAnand@users.noreply.github.com>
2021-04-04 09:42:42 -0700

    added more options

--- forms.py
+++ forms.py
@@ -3,13 +3,15 @@ from wtforms import SelectField, StringField
 from wtforms.validators import InputRequired
 
 class CreateVendorForm(FlaskForm):
-    organic = SelectField('Is your produce organic?', choices=['Yes', 'No', 'Somewhat', 'N/A'], validators=[InputRequired()])
+    organic = SelectField('Is your produce organic?', choices=['Yes', 'No', 'Somewhat', 'N/A'],
+                          validators=[InputRequired()])
     price = SelectField('How expensive are your goods?', choices=['$', '$$', '$$$'], validators=[InputRequired()])
     name = StringField('Business Name', validators=[InputRequired()])
     description = StringField('Description', validators=[InputRequired()])
-    methods = SelectField('What are your methods of farming?', choices=['Sustainable', 'Unsustainable'], validators=[InputRequired()])
-    chemical = SelectField('Do you use chemicals?', choices=['Yes', 'No'], validators=[InputRequired()])
-    usda = SelectField('Have you received USDA approval?', choices=['Yes', 'No'], validators=[InputRequired()])
+    methods = SelectField('What are your methods of farming?', choices=['Sustainable', 'Unsustainable', 'N/A'],
+                          validators=[InputRequired()])
+    chemical = SelectField('Do you use chemicals?', choices=['Yes', 'No', 'N/A'], validators=[InputRequired()])
+    usda = SelectField('Have you received USDA approval?', choices=['Yes', 'No', 'N/A'], validators=[InputRequired()])
     contact = StringField('Contact Information', validators=[InputRequired()])
     produce = StringField('Items Available', validators=[InputRequired()])
 
