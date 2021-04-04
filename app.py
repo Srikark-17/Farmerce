801ca1d28c07293169e8f07709b92f9bf247369b
Sidharth Anand <44172551+SidharthAnand@users.noreply.github.com>
2021-04-04 16:31:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,29 +20,41 @@ def index():
 @app.route('/new_vendor', methods=['GET', 'POST'])
 def create_vendor():
     form = CreateVendorForm()
+    collection = db.vendors
 
     if request.method == 'POST':
         print(form.name.data)
         print(form.description.data)
-        print(form.price.data)
+        print(form.pic.data)
 
-        post = {'id': uuid.uuid4().hex, 'name': form.name.data, 'description': form.description.data, 'is_organic': form.organic.data,
-                'farming_method': form.methods.data, 'is_chemical': form.chemical.data, 'usda': form.usda.data,
-                'contact': form.contact.data, 'price': form.price.data, 'produce': form.produce.data}
+        post = {'id': uuid.uuid4().hex, 'name': form.name.data, 'description': form.description.data,
+                'is_organic': form.organic.data, 'farming_method': form.methods.data,
+                'is_chemical': form.chemical.data, 'usda': form.usda.data, 'contact': form.contact.data,
+                'price': form.price.data, 'produce': form.produce.data, 'pic': form.pic.data}
 
-        collection = db.vendors
         collection.insert_one(post)
+
         return redirect(url_for('index'))
 
     return render_template('new_vendor.html', form=form)
 
 @app.route('/view')
 def view():
-    return 'to be implemented'
+    collection = db.vendors
+    data = reversed(list(collection.find()))
+    return render_template('gallery.html', data=data)
 
 @app.route('/view/<id>')
 def view_vendor(id):
-    return 'to be implemented '
+    collection = db.vendors
+
+    data = list(collection.find())
+    vendor = {}
+    for x in data:
+        if x['id'] == id:
+            vendor = x
+
+    return render_template('viewvendor.html', vendor=vendor)
 
 if __name__ == "__main__":
     app.run(debug=True)
