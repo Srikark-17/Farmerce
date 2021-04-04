41969d2c5a823e94c0a1bf43072f87dd4dae8e52
Sidharth Anand <44172551+SidharthAnand@users.noreply.github.com>
2021-04-04 09:54:25 -0700

    more changes

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ app.config['SECRET_KEY'] = 'secret'
 cluster = MongoClient("mongodb+srv://sid:"+password+"@farmerce.2s4c2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster.farmerce
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/new_vendor', methods=['GET', 'POST'])
 def create_vendor():
     form = CreateVendorForm()
 
@@ -26,10 +32,17 @@ def create_vendor():
 
         collection = db.vendors
         collection.insert_one(post)
-        # return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
     return render_template('new_vendor.html', form=form)
 
+@app.route('/view')
+def view():
+    return 'to be implemented'
+
+@app.route('/view/<id>')
+def view_vendor(id):
+    return 'to be implemented '
 
 if __name__ == "__main__":
     app.run(debug=True)
